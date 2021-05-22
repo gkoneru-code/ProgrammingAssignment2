10284b2c95adeb2198c31273ed9afb147bb3de2b
gkoneru-code <geethikakoneru@gmail.com>
2021-05-22 14:47:32 +0530

    Programming Assignment2 - a function which creates an inversible matrix, and a function which returns the inverse matrix

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,28 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m<-NULL
+     set <- function(y) {
+         x <<- y
+         m <<- NULL
+       }
+     get<- function() x
+     setmatrixinv<- function(inverse) m<<-inverse
+     getmatrixinv<- function() m
+     list( set=set, get=get, setmatrixinv=setmatrixinv, getmatrixinv=getmatrixinv)
 }
 
-
-## Write a short comment describing this function
+##This function creates an inversible matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m<-x$getmatrixinv()
+     if(!is.null(m)) {
+         message("getting cached data")
+         return (m)
+       }
+     data<- x$get()
+     m<- solve(data, ...)
+     x$setmatrixinv(m)
+     m ## Return a matrix that is the inverse of 'x'
 }
+##This function returns the inverse of a matrix
